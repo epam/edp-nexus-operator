96f0bfeffcca584d81faffa9c499c27cbdd93fc9
Pavlo_Yemelianov <pavlo_yemelianov@epam.com>
2018-05-03 13:21:27 +0300

    Add api prefix for url

--- src/main/java/com/epam/edp/demo/conroller/HelloEdpController.java
+++ src/main/java/com/epam/edp/demo/conroller/HelloEdpController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloEdpController {
 
-    @GetMapping(value = "/hello")
+    @GetMapping(value = "/api/hello")
     public String hello() {
         return "Hello, EDP!";
     }
