8a65401d2278b5a0e3417c30c9bc37c732944bdb
Alexander Morozov <alexander_morozov@epam.com>
2019-08-07 17:21:22 +0300

    [EPMDEDP-2965] Implement functionality to check if task exist
    Change-Id: Ic791074d40d1a11d361f5155a7db1eff9170bfd3

--- pkg/client/nexus/nexus.go
+++ pkg/client/nexus/nexus.go
@@ -20,6 +20,7 @@ type NexusClient struct {
 	ApiUrl   string
 }
 
+// InitNewRestClient performs initialization of Nexus connection
 func (nc *NexusClient) InitNewRestClient(instance *v1alpha1.Nexus, url string, user string, password string) error {
 	nc.resty = *resty.SetHostURL(url).SetBasicAuth(user, password)
 	nc.instance = instance
@@ -27,6 +28,7 @@ func (nc *NexusClient) InitNewRestClient(instance *v1alpha1.Nexus, url string, u
 	return nil
 }
 
+// WaitForStatusIsUp waits for Nexus to be up
 func (nc NexusClient) WaitForStatusIsUp(retryCount int, timeout time.Duration) error {
 	resp, err := nc.resty.
 		SetRetryCount(retryCount).
@@ -47,6 +49,7 @@ func (nc NexusClient) WaitForStatusIsUp(retryCount int, timeout time.Duration) e
 	return nil
 }
 
+// CheckScriptExist checks if script is already uploaded
 func (nc NexusClient) CheckScriptExist(scriptName string) (bool, error) {
 	resp, err := nc.resty.R().
 		SetHeader("accept", "application/json").
@@ -65,6 +68,7 @@ func (nc NexusClient) CheckScriptExist(scriptName string) (bool, error) {
 	return false, nil
 }
 
+// UploadScript uploads script to Nexus
 func (nc NexusClient) UploadScript(scriptName string, scriptType string, scriptPath string) error {
 	scriptContent, err := ioutil.ReadFile(scriptPath)
 	formattedContent := nexusClientHelper.FormateNexusScript(string(scriptContent))
@@ -78,6 +82,7 @@ func (nc NexusClient) UploadScript(scriptName string, scriptType string, scriptP
 	return nil
 }
 
+// AreDefaultScriptsDeclared checks if default scripts are already declared in Nexus
 func (nc NexusClient) AreDefaultScriptsDeclared(scriptsPath string) (bool, error) {
 	defaultScriptsAreDeclared := true
 	files, err := ioutil.ReadDir(scriptsPath)
@@ -98,6 +103,7 @@ func (nc NexusClient) AreDefaultScriptsDeclared(scriptsPath string) (bool, error
 	return defaultScriptsAreDeclared, nil
 }
 
+// DeclareDefaultScripts declares default scripts in Nexus
 func (nc NexusClient) DeclareDefaultScripts(scriptsPath string) error {
 	files, err := ioutil.ReadDir(scriptsPath)
 	if err != nil {
@@ -119,4 +125,23 @@ func (nc NexusClient) DeclareDefaultScripts(scriptsPath string) error {
 		}
 	}
 	return nil
+}
+
+// CheckScriptExist checks if task is already uploaded
+func (nc NexusClient) CheckTaskExist(taskName string) (bool, error) {
+	resp, err := nc.resty.R().
+		SetHeader("accept", "application/json").
+		Get("/tasks")
+	if err != nil || resp.IsError() {
+		return false, helper.LogErrorAndReturn(errors.New(fmt.Sprintf("Checking if task %v exist failed. Err - %v. Response - %s", taskName, err, resp.Status())))
+	}
+
+	var tasksList map[string][]map[string]interface{}
+	err = json.Unmarshal(resp.Body(), &tasksList)
+	for _, task := range tasksList["items"] {
+		if task["name"] == taskName {
+			return true, nil
+		}
+	}
+	return false, nil
 }
