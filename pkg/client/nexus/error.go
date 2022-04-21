ab466d42a608dbbf1d2ad38ce3e713433a9ea332
Mykhailo Bolsunovskyi <mykhailo_bolsunovskyi@epam.com>
2022-04-21 15:40:07 +0300

    [EPMDEDP-8941]: feat: Check nexus user exists before creation
    Change-Id: I5d2bdb350d7fa4b91396a45f9fa46422e84143f1

---
+++ pkg/client/nexus/error.go
@@ -0,0 +1,36 @@
+package nexus
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+type ErrNotFound string
+
+func (e ErrNotFound) Error() string {
+	return string(e)
+}
+
+func IsErrNotFound(err error) bool {
+	_, ok := errors.Cause(err).(ErrNotFound)
+	return ok
+}
+
+type HTTPError struct {
+	code    int
+	message string
+}
+
+func (e HTTPError) Error() string {
+	return fmt.Sprintf("status: %d, body: %s", e.code, e.message)
+}
+
+func IsHTTPErrorCode(err error, code int) bool {
+	httpError, ok := errors.Cause(err).(HTTPError)
+	if !ok {
+		return false
+	}
+
+	return httpError.code == code
+}
--- pkg/client/nexus/user_test.go
+++ pkg/client/nexus/user_test.go
@@ -4,69 +4,117 @@ import (
 	"context"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/suite"
 	"gopkg.in/resty.v1"
 )
 
-func initClient() *Client {
-	cl := Client{
+type ClientTestSuite struct {
+	suite.Suite
+	cl *Client
+}
+
+func TestClientTestSuite(t *testing.T) {
+	suite.Run(t, new(ClientTestSuite))
+}
+
+func (s *ClientTestSuite) SetupTest() {
+	s.cl = &Client{
 		resty: resty.New(),
 	}
 
-	httpmock.ActivateNonDefault(cl.resty.GetClient())
-
-	return &cl
+	httpmock.Reset()
+	httpmock.ActivateNonDefault(s.cl.resty.GetClient())
 }
 
-func TestClient_CreateUser(t *testing.T) {
-	cl := initClient()
+func (s *ClientTestSuite) TestClient_CreateUser() {
+	t := s.T()
 
 	httpmock.RegisterResponder("POST", "/security/users", httpmock.NewStringResponder(200, ""))
-	if err := cl.CreateUser(context.Background(), &User{
+	err := s.cl.CreateUser(context.Background(), &User{
 		Email: "mktest@example.com",
-	}); err != nil {
-		t.Fatal(err)
-	}
+	})
+	assert.NoError(t, err)
 
 	httpmock.RegisterResponder("POST", "/security/users",
 		httpmock.NewStringResponder(400, "bad request"))
 
-	err := cl.CreateUser(context.Background(), &User{})
-	if err == nil || !IsHTTPErrorCode(err, 400) {
-		t.Fatalf("wrong error returned: %+v", err)
-	}
+	err = s.cl.CreateUser(context.Background(), &User{})
+	assert.Error(t, err)
+	assert.True(t, IsHTTPErrorCode(err, 400))
 }
 
-func TestClient_UpdateUser(t *testing.T) {
-	cl := initClient()
+func (s *ClientTestSuite) TestClient_UpdateUser() {
+	t := s.T()
 
 	httpmock.RegisterResponder("PUT", "/security/users/id1", httpmock.NewStringResponder(200, ""))
-	if err := cl.UpdateUser(context.Background(), &User{Email: "mktest1@example.com", ID: "id1"}); err != nil {
-		t.Fatal(err)
-	}
+	err := s.cl.UpdateUser(context.Background(), &User{Email: "mktest1@example.com", ID: "id1"})
+	assert.NoError(t, err)
 
 	httpmock.RegisterResponder("PUT", "/security/users/id1",
 		httpmock.NewStringResponder(400, "bad request"))
-	err := cl.UpdateUser(context.Background(), &User{Email: "mktest1@example.com", ID: "id1"})
-	if err == nil || !IsHTTPErrorCode(err, 400) {
-		t.Fatalf("wrong error returned: %+v", err)
-	}
+	err = s.cl.UpdateUser(context.Background(), &User{Email: "mktest1@example.com", ID: "id1"})
+	assert.Error(t, err)
+	assert.True(t, IsHTTPErrorCode(err, 400))
 }
 
-func TestClient_DeleteUser(t *testing.T) {
-	cl := initClient()
+func (s *ClientTestSuite) TestClient_DeleteUser() {
+	t := s.T()
 
 	httpmock.RegisterResponder("DELETE", "/security/users/id1",
 		httpmock.NewStringResponder(200, ""))
-	if err := cl.DeleteUser(context.Background(), "id1"); err != nil {
-		t.Fatal(err)
-	}
+	err := s.cl.DeleteUser(context.Background(), "id1")
+	assert.NoError(t, err)
 
 	httpmock.RegisterResponder("DELETE", "/security/users/id2",
 		httpmock.NewStringResponder(500, ""))
-	err := cl.DeleteUser(context.Background(), "id2")
+	err = s.cl.DeleteUser(context.Background(), "id2")
+	assert.Error(t, err)
+	assert.True(t, IsHTTPErrorCode(err, 500))
+}
 
-	if err == nil || !IsHTTPErrorCode(err, 500) {
-		t.Fatalf("wrong error returned: %+v", err)
-	}
+func (s *ClientTestSuite) TestClient_GetUsers() {
+	t := s.T()
+
+	httpmock.RegisterResponder("GET", "/security/users", httpmock.NewJsonResponderOrPanic(200, []User{
+		{
+			Email: "mk@example.com",
+		},
+	}))
+	users, err := s.cl.GetUsers(context.Background())
+
+	assert.NoError(t, err)
+	assert.True(t, len(users) > 0)
+	assert.Equal(t, users[0].Email, "mk@example.com")
+
+	httpmock.RegisterResponder("GET", "/security/users", httpmock.NewStringResponder(500, ""))
+	_, err = s.cl.GetUsers(context.Background())
+	assert.Error(t, err)
+	assert.True(t, IsHTTPErrorCode(err, 500))
+}
+
+func (s *ClientTestSuite) TestClient_GetUser() {
+	t := s.T()
+	email := "mk@gmail.com"
+
+	httpmock.RegisterResponder("GET", "/security/users", httpmock.NewJsonResponderOrPanic(200, []User{
+		{
+			Email: email,
+		},
+	}))
+
+	usr, err := s.cl.GetUser(context.Background(), email)
+	assert.NoError(t, err)
+	assert.Equal(t, usr.Email, email)
+
+	usr, err = s.cl.GetUser(context.Background(), "wrong_email")
+	assert.Error(t, err)
+	assert.True(t, IsErrNotFound(err))
+
+	httpmock.RegisterResponder("GET", "/security/users", httpmock.NewStringResponder(500, ""))
+	_, err = s.cl.GetUser(context.Background(), "e")
+	assert.Error(t, err)
+	assert.True(t, IsHTTPErrorCode(err, 500))
 }
